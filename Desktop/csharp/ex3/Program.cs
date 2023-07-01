6d39dae13662512ab35f5efb6c0bf3d5bccc577a
Artyom <hubabuba0144@gmail.com>
2023-07-01 22:36:30 +0300

    numbers

--- Desktop/csharp/ex3/Program.cs
+++ Desktop/csharp/ex3/Program.cs
@@ -1,10 +1,11 @@
-﻿Console.WriteLine("Hello, who are you?");
-string name = Console.ReadLine();
-if (name.ToLower() == "artyom")
-{
-    Console.WriteLine("Good morning, sweetheart!");
-}
-else
-{
-    Console.WriteLine("Good morning!");
-}
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 6;
+int max = a;
+if (max <b) max = b;
+if (max <c) max = c;
+if (max <d) max = d;
+if (max <e) max = e;
+Console.WriteLine(max);
